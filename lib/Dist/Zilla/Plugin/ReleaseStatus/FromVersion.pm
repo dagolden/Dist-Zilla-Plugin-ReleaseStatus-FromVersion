3654ad6755994206825826b11cf41d1ad3c76c73
David Golden <dagolden@cpan.org>
2015-03-21 04:15:24 -0400

    initial import

---
+++ lib/Dist/Zilla/Plugin/ReleaseStatus/FromVersion.pm
@@ -0,0 +1,34 @@
+use v5.10;
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::ReleaseStatus::FromVersion;
+# ABSTRACT: Set release status from version number patterns
+
+our $VERSION = '0.001';
+
+1;
+
+=for Pod::Coverage BUILD
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::ReleaseStatus::FromVersion;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+=for :list
+* Maybe other modules do related things.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et tw=75:
